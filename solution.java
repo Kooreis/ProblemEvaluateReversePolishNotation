ac899b7a29113398d4e5b1ba270c481418fedc63
Kooreis <kooreis.official@gmail.com>
2023-07-20 10:00:55 -0500

    The main method is created. It contains a string array of tokens that represent a Reverse Polish Notation expression. The evalRPN method is called with the tokens as an argument, and the result is printed to the console.

--- solution.java
+++ solution.java
@@ -1,6 +1,3 @@
-Here is a Java console application that evaluates an arithmetic expression in Reverse Polish Notation:
-
-```java
 import java.util.Stack;
 
 public class Main {
@@ -8,35 +5,4 @@ public class Main {
         String[] tokens = new String[] { "2", "1", "+", "3", "*" };
         System.out.println(evalRPN(tokens));
     }
-
-    public static int evalRPN(String[] tokens) {
-        Stack<Integer> stack = new Stack<>();
-        for (String token : tokens) {
-            if (!"+-*/".contains(token)) {
-                stack.push(Integer.valueOf(token));
-                continue;
-            }
-
-            int number2 = stack.pop();
-            int number1 = stack.pop();
-            switch (token) {
-                case "+":
-                    stack.push(number1 + number2);
-                    break;
-                case "-":
-                    stack.push(number1 - number2);
-                    break;
-                case "*":
-                    stack.push(number1 * number2);
-                    break;
-                case "/":
-                    stack.push(number1 / number2);
-                    break;
-            }
-        }
-        return stack.pop();
-    }
 }
-```
-
-This program evaluates the Reverse Polish Notation expression "2 1 + 3 *", which equals 9. The `evalRPN` method uses a stack to keep track of the numbers in the expression. When it encounters an operator, it pops the top two numbers from the stack, performs the operation, and pushes the result back onto the stack. At the end of the expression, the final result is the only number left on the stack.
