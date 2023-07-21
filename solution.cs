b587a684761379968595466e71018f3fc64fd22e
Kooreis <kooreis.official@gmail.com>
2023-07-21 10:00:55 -0500

    Creation of the Main method, which prompts the user for input, reads the input, and then calls the EvaluateRPN method with the input.

--- solution.cs
+++ solution.cs
@@ -1,6 +1,6 @@
-using System;
-using System.Collections.Generic;
-
-class Program
+static void Main(string[] args)
 {
+Console.WriteLine("Enter Reverse Polish Notation expression (separated by space):");
+string input = Console.ReadLine();
+Console.WriteLine("Result: " + EvaluateRPN(input));
 }
